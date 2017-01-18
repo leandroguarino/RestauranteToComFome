4ca15a94a296a41533d315c3546a0c82c32083d8
leandroguarino <le.guarino@gmail.com>
2017-01-17 23:12:26 -0200

    Correcao do VendaController da aula 14

--- RestauranteToComFome/grails-app/controllers/restaurantetocomfome/VendaController.groovy
+++ RestauranteToComFome/grails-app/controllers/restaurantetocomfome/VendaController.groovy
@@ -12,18 +12,22 @@ class VendaController {
     }
 
     def salvar(){
+        //recebe os ids dos produtos
         def produto = params.produto
+        //recebe as quantidades digitadas pelo usuário
         def quantidade = params.quantidade
 
-        println produto.class.simpleName
-
         def mensagem = [:]
+        //este try...catch é usado para receber possíveis Exceptions do VendaService
         try {
+            //chama o service para salvar a venda (pedido)
             vendaService.salvar(params.nome, params.email, params.produto.toList(), params.quantidade.toList())
             mensagem += ["resposta":"OK"]
         }catch(Exception excecao){
+            //se o método salvar do Service retornar alguma exceção, a mensagem da exceção será enviada do controller para a view
             mensagem += ["resposta":excecao.message]
         }
+        //envia o mapa "mensagem" para a view
         render mensagem as JSON
     }
 }
